eebe6e30894eb088b0c1910db989afff79ebbadd
bfog <brian.fogarty98@gmail.com>
2017-12-18 22:37:00 +1100

    added list item fade out and removal

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,3 +1,11 @@
 $("li").click(function(){
    $(this).toggleClass("completed");
 });
+
+$("span").click(function(e){
+   $(this).parent().fadeOut(500, function(){
+      $(this).remove();
+   });
+   //stop event from bubbling up
+   e.stopPropagation();
+});
